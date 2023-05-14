e655a5a94d19a11c78699f23d842244f0a088e33
ndelucca <nazadelucca@gmail.com>
2023-05-13 23:03:09 -0300

    cow now presents itself

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import { fetchFortuneCookie } from "./modules/fortune";
 import "./scss/main.scss";
 
 async function lucky_cow() {
+  document.querySelector(".cowsie").innerHTML = randomCow("Moo..?");
   const fortune_cookie = await fetchFortuneCookie();
-  document.querySelector(".cowsie").innerHTML = `<pre>${randomCow(fortune_cookie)}</pre>`;
+  document.querySelector(".cowsie").innerHTML = randomCow(fortune_cookie);
 }
 
 window.addEventListener("load", lucky_cow);
