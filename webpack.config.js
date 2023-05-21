719a02ddfefe2bef60472923ed6c292e3effaa64
ndelucca <nazadelucca@gmail.com>
2023-05-21 01:51:20 -0300

    cow optimizes page loading
    doesn't give up on automatic markdown loading...

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
+const webpack = require("webpack");
 const path = require("path");
 const fs = require("fs");
-
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const build_dir = "build";
-const pages = ["index", "fountain", "ephemerides"];
+
+function listFiles(directory, extension) {
+  const files = fs.readdirSync(directory);
+  const filesWithoutExtension = files
+    .filter((file) => path.extname(file) === extension)
+    .map((file) => path.basename(file, extension));
+  return filesWithoutExtension;
+}
+
+j2pages = listFiles("./src/templates/pages/", ".j2");
+mdPages = listFiles("./md/", ".md");
 
 const entryPoints = {
   main: ["./src/scss/main.scss"],
@@ -24,10 +34,11 @@ const plugins = [
   }),
 ];
 
+const globals = {};
 // Cada página:
 //  tiene su propio bundle a partir de un .ts específico.
 //  utiliza su propio template jinja2
-pages.map((page_name) => {
+j2pages.forEach((page_name) => {
   entryPoints[page_name] = [];
 
   if (fs.existsSync(`./src/ts/${page_name}.ts`)) {
@@ -40,12 +51,39 @@ pages.map((page_name) => {
 
   plugins.push(
     new HtmlWebpackPlugin({
-      template: `!!html-loader!jinja2-loader!src/templates/${page_name}.j2`,
+      template: `!!html-loader!jinja2-loader!src/templates/pages/${page_name}.j2`,
       filename: `${page_name}.html`,
       chunks: ["main", page_name],
     })
   );
 });
+
+mdPages.forEach((page_name) => {
+  entryPoints[page_name] = [];
+
+  if (fs.existsSync(`./src/ts/markdown.ts`)) {
+    entryPoints[page_name].push(`./src/ts/markdown.ts`);
+  }
+  if (fs.existsSync(`./src/scss/markdown.scss`)) {
+    entryPoints[page_name].push(`./src/scss/markdown.scss`);
+  }
+
+  globals[page_name] = fs.readFileSync(`./md/${page_name}.md`, "utf-8");
+
+  plugins.push(
+    new HtmlWebpackPlugin({
+      template: `!!html-loader!jinja2-loader!src/templates/markdown.j2`,
+      filename: `${page_name}.html`,
+      chunks: ["main", page_name],
+    })
+  );
+});
+
+plugins.push(
+  new webpack.DefinePlugin({
+    globals: JSON.stringify(globals),
+  })
+);
 
 module.exports = {
   entry: entryPoints,
